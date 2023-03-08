d4a3a53eddada6ca32684e3efc1654d82aa0b524
vaishnavikolewar <vkolewar@gmail.com>
2023-03-08 13:28:25 +0000

    solution

---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,15 @@
+package com.example.recipe;
+
+import java.util.*;
+
+public interface RecipeRepository {
+    ArrayList<Recipe> getAllRecipes();
+
+    Recipe getRecipeById(int recipeId);
+
+    Recipe addRecipe(Recipe recipe);
+
+    Recipe updateRecipe(int recipeId, Recipe recipe);
+
+    void deleteRecipe(int recipeId);
+}
